19c26d6395a0fdcd1274082273b209ce34dfabe7
rahulkul7218 <kulkarnirahul134@gmail.com>
2025-11-05 10:26:16 +0530

    item master and bom screen customization

--- aims_customization/hooks.py
+++ aims_customization/hooks.py
@@ -253,7 +253,10 @@ after_migrate = [
     "aims_customization.patches.v_0.add_short_close_reason_field.execute",
     "aims_customization.patches.v_0.add_mould_details_tab_on_item.execute",
     # "aims_customization.patches.v_0.dia_field_mandatory_when_item_group_toolroolrm_round.execute"
-
+    "aims_customization.patches.v_0.add_part_specification_tab_on_item.execute",
+    "aims_customization.patches.v_0.add_packing_details_tab_on_item.execute",
+    "aims_customization.patches.v_0.add_section_on_bom.execute"
+    
     
     
     
